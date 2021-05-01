56447f78d0663bb70f48493dab6f28ab185ff794
paroar <paroar@pop-os.localdomain>
2021-05-01 16:00:00 +0200

    patch & delete politicians

--- nodejs/router/politicians.js
+++ nodejs/router/politicians.js
@@ -4,11 +4,17 @@ import Politician from "./../models/Politicians.js"
 const politiciansRouter = Router();
 
 politiciansRouter.get("/", async (req, res) => {
+    const {page = 0, limit = 50} = req.query
     try {
-        const politicians = await Politician.find().limit(50)
-        res.json(politicians);
+        const politicians = await Politician.find().skip(page*limit).limit(limit)
+        const count = await Politician.countDocuments();
+        res.json({
+            politicians, 
+            totalPages: Math.ceil(count/limit) - 1, 
+            currentPage: page
+        });
     } catch (error) {
-        res.statusCode(400).json({ message: error })
+        res.status(400).json({ message: error })
     }
 });
 
@@ -17,26 +23,34 @@ politiciansRouter.get("/:politicianId", async (req, res) => {
         const politician = await Politician.findById(req.params.politicianId)
         res.json(politician);
     } catch (error) {
-        res.statusCode(400).json({ message: error })
+        res.status(400).json({ message: error })
     }
 });
 
 politiciansRouter.post("/", async (req, res) => {
     const politician = new Politician({
-        // name: req.body.name,
-        // party: req.body.party,
-        // sex: req.body.sex,
-        // charge: req.body.charge,
-        // institution: req.body.institution,
-        // ccaa: req.body.ccaa,
-        // salary: req.body.salary,
+        CARGO: req.body.CARGO,
+        CARGO_PARA_FILTRO: req.body.CARGO_PARA_FILTRO,
+        CCAA: req.body.CCAA,
+        COMPLEMENTOS_SUELDO: req.body.COMPLEMENTOS_SUELDO,
+        GENERO: req.body.GENERO,
+        INSTITUCION: req.body.INSTITUCION,
+        OTRASDIETASEINDEMNIZACIONES_SUELDO: req.body.OTRASDIETASEINDEMNIZACIONES_SUELDO,
+        PAGASEXTRA_SUELDO: req.body.PAGASEXTRA_SUELDO,
+        PARTIDO: req.body.PARTIDO,
+        PARTIDO_PARA_FILTRO: req.body.PARTIDO_PARA_FILTRO,
+        RETRIBUCIONANUAL: req.body.RETRIBUCIONANUAL,
+        RETRIBUCIONMENSUAL: req.body.RETRIBUCIONMENSUAL,
+        SUELDOBASE_SUELDO: req.body.SUELDOBASE_SUELDO,
+        TITULAR: req.body.TITULAR,
+        TRIENIOS_SUELDO: req.body.TRIENIOS_SUELDO,
     })
 
     try {
         const savedPolitician = await politician.save()
         res.json(savedPolitician)
     } catch (error) {
-        res.statusCode(400).json({ message: error })
+        res.status(400).json({ message: error })
     }
 })
 
@@ -46,18 +60,26 @@ politiciansRouter.patch("/:politicianId", async (req, res) => {
             { _id: req.params.politicianId },
             {
                 $set: {
-                    // name: req.body.name,
-                    // party: req.body.party,
-                    // sex: req.body.sex,
-                    // charge: req.body.charge,
-                    // institution: req.body.institution,
-                    // ccaa: req.body.ccaa,
-                    // salary: req.body.salary,
+                    CARGO: req.body.CARGO,
+                    CARGO_PARA_FILTRO: req.body.CARGO_PARA_FILTRO,
+                    CCAA: req.body.CCAA,
+                    COMPLEMENTOS_SUELDO: req.body.COMPLEMENTOS_SUELDO,
+                    GENERO: req.body.GENERO,
+                    INSTITUCION: req.body.INSTITUCION,
+                    OTRASDIETASEINDEMNIZACIONES_SUELDO: req.body.OTRASDIETASEINDEMNIZACIONES_SUELDO,
+                    PAGASEXTRA_SUELDO: req.body.PAGASEXTRA_SUELDO,
+                    PARTIDO: req.body.PARTIDO,
+                    PARTIDO_PARA_FILTRO: req.body.PARTIDO_PARA_FILTRO,
+                    RETRIBUCIONANUAL: req.body.RETRIBUCIONANUAL,
+                    RETRIBUCIONMENSUAL: req.body.RETRIBUCIONMENSUAL,
+                    SUELDOBASE_SUELDO: req.body.SUELDOBASE_SUELDO,
+                    TITULAR: req.body.TITULAR,
+                    TRIENIOS_SUELDO: req.body.TRIENIOS_SUELDO,
                 }
             })
         res.json(updatedPolitician)
     } catch (error) {
-        res.statusCode(400).json({ message: error })
+        res.status(400).json({ message: error })
     }
 })
 
@@ -67,7 +89,7 @@ politiciansRouter.delete("/:politicianId", async (req, res) => {
         const removedPolitician = await Politician.deleteOne({ _id: req.params.politicianId })
         res.json(removedPolitician);
     } catch (error) {
-        res.statusCode(400).json({ message: error })
+        res.status(400).json({ message: error })
     }
 })
 
