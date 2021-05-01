de387436a401a6879fa76361154015137b13e70f
paroar <paroar@pop-os.localdomain>
2021-05-01 12:22:24 +0200

    politician model

---
+++ nodejs/models/Politicians.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose"
+
+const PoliticianSchema = mongoose.Schema({
+    TITULAR: {
+        type: String,
+        required: true
+    },
+    PARTIDO: {
+        type: String,
+        required: true
+    },
+    PARTIDO_PARA_FILTRO: {
+        type: String,
+        required: true
+    },
+    GENERO: {
+        type: String,
+        required: true
+    },
+    CARGO_PARA_FILTRO: {
+        type: String,
+        required: true
+    },
+    CARGO: {
+        type: String,
+        required: true
+    },
+    INSTITUCION: {
+        type: String,
+        required: true
+    },
+    CCAA: {
+        type: String,
+        required: true
+    },
+    SUELDOBASE_SUELDO: {
+        type: Number,
+        required: true
+    },
+    COMPLEMENTOS_SUELDO: {
+        type: Number,
+        required: true
+    },
+    PAGASEXTRA_SUELDO: {
+        type: Number,
+        required: true
+    },
+    OTRASDIETASEINDEMNIZACIONES_SUELDO: {
+        type: Number,
+        required: true
+    }
+    ,
+    TRIENIOS_SUELDO: {
+        type: Number,
+        required: true
+    }
+    ,
+    RETRIBUCIONMENSUAL: {
+        type: Number,
+        required: true
+    }
+    ,
+    RETRIBUCIONANUAL: {
+        type: Number,
+        required: true
+    }
+    ,
+    OBSERVACIONES: {
+        type: Number,
+        required: true
+    }
+}, { collection: 'politicians' })
+
+export default mongoose.model("Politician", PoliticianSchema)
